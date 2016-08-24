af613c48349ae63e803adbb9fb0ee48b905659e0
Nestor Torres <nchamorro@uninorte.edu.co>
2016-08-24 04:19:22 +0000

    orangebox finished

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,24 @@ gulp.task('clean',function(){
     return del(['dist/**/*']);
 });
 
-gulp.task('build',['clean','sass','images'],function(){
+gulp.task('build',['clean','sass','images','fonts'],function(){
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', ['clean'], function () {
   return gulp.src('src/sass/**/*.scss') 
   .pipe(sass({includePaths: require('node-normalize-scss').includePaths}))
   .pipe(gulp.dest('dist/css'));
 });
  
- gulp.task('images', function(){
+ gulp.task('images', ['clean'], function(){
   gulp.src('src/images/**/*')
     .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
     .pipe(gulp.dest('dist/images/'));
+});
+
+gulp.task('fonts', ['clean'], function(){
+  return gulp.src('src/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
 });
 
  gulp.task('default',['watch']);
